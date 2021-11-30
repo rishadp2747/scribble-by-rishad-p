134e57a012a112e99d1a4abb164586a501e838bb
rishadp <rishadp2747@gmail.com>
2021-11-30 13:17:30 +0530

    removed unwanted gems and fixtures

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +18,8 @@ module Scribble
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.test_framework :test_unit, fixture: false
+    end
   end
 end
