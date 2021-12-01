3501525e5b1b0dd2d800f5f01bd1fe122963a50f
rishadp <rishadp2747@gmail.com>
2021-12-01 20:16:33 +0530

    Added test for category model

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -3,7 +3,17 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @category = create(:category)
+  end
+
+  def test_category_should_be_valid
+    assert @category.valid?
+  end
+
+  def test_category_should_not_be_valid_without_title
+    @category.title = ""
+    assert_not @category.valid?
+    assert_includes @category.errors.full_messages, "Title can't be blank"
+  end
 end
