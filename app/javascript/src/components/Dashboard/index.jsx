d0d8441830f4e1d193ba60b00a980a97f31d4bf6
rishadp <rishadp2747@gmail.com>
2021-12-01 15:15:59 +0530

    Added tables and column

--- app/javascript/src/components/Dashboard/index.jsx
+++ app/javascript/src/components/Dashboard/index.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Plus, Search } from "neetoicon";
-import { Typography, Dropdown, Button, Input, Checkbox } from "neetoui";
-import { MenuBar } from "neetoui/layouts";
+import { Plus, Search, Highlight, Delete } from "neetoicon";
+import { Typography, Dropdown, Button, Input, Checkbox, Table } from "neetoui";
+import {
+  MenuBar,
+  Scrollable,
+  Container as PageContainer,
+} from "neetoui/layouts";
 
 import Container from "components/Common/Container";
+import { DEFAULT_TABLE_COLUMNS } from "components/Dashboard/constant";
+
+const DataActions = () => {
+  return (
+    <div className="flex flex-row space-x-4">
+      <Delete size={16} />
+      <Highlight size={16} />
+    </div>
+  );
+};
 
 const Dashboard = () => {
+  const [tableColumns, setTableColumns] = useState(DEFAULT_TABLE_COLUMNS);
+
   const ACTION_ICONS = [
     {
       icon: Search,
@@ -16,11 +32,42 @@ const Dashboard = () => {
     },
   ];
 
-  const TABLE_COLUMNS = ["Title", "Categories", "Date", "Author", "Status"];
+  const CUSTOM_TABLE_COLUMNS = tableColumns
+    .filter(({ show }) => show)
+    .map(({ data }) => data);
+
+  const TABLE_COLUMNS = [
+    ...CUSTOM_TABLE_COLUMNS,
+    {
+      dataIndex: "edit",
+      key: "edit",
+      render: () => <DataActions />,
+      width: 20,
+    },
+  ];
+
+  const data = [
+    {
+      title: "Welcome to Scribble",
+      date: "October 9th, 2022",
+      author: "Oliver Smith",
+      status: "Published",
+      category: "Getting Started",
+    },
+    {
+      title: "Welcome to Scribble",
+      date: "October 9th, 2022",
+      author: "Oliver Smith",
+      status: "Published",
+      category: "Getting Started",
+    },
+  ];
+
+  setTableColumns([]);
 
   return (
     <Container>
-      <div className="flex">
+      <div className="flex w-screen">
         <MenuBar showMenu={true} title="Articles">
           <MenuBar.Block label="All" count={67} active />
           <MenuBar.Block label="Draft" count={15} />
@@ -42,43 +89,54 @@ const Dashboard = () => {
           <MenuBar.Block label="Security & Privacy" count={60} />
           <MenuBar.Block label="Misc" count={60} />
         </MenuBar>
-        <div className="flex flex-col w-full p-4 border border-green-500">
-          <div className="flex flex-row w-4/5 h-8 ml-auto space-x-3">
-            <Input
-              size="small"
-              prefix={<Search size={20} />}
-              placeholder="Search article title"
-            />
-            <Dropdown
-              label="Columns"
-              buttonStyle="text"
-              position="bottom-end"
-              className="h-8 bg-gray-200 border"
-            >
-              <div className="p-4 space-y-4">
-                <Typography style="h5">Columns</Typography>
-                {TABLE_COLUMNS.map((column, index) => (
-                  <Checkbox
-                    checked
-                    key={index}
-                    label={column}
-                    id={column.toLocaleLowerCase()}
-                  />
-                ))}
-              </div>
-            </Dropdown>
-            <Button
-              icon={Plus}
-              iconPosition="right"
-              label="Add New Article"
-              className="bg-indigo-500"
-            />
-          </div>
+        <PageContainer>
+          <div className="flex flex-col w-full py-4">
+            <div className="flex flex-row w-4/5 h-8 ml-auto space-x-3">
+              <Input
+                size="small"
+                prefix={<Search size={20} />}
+                placeholder="Search article title"
+              />
+              <Dropdown
+                label="Columns"
+                buttonStyle="text"
+                position="bottom-end"
+                className="h-8 bg-gray-200 border"
+              >
+                <div className="p-4 space-y-4">
+                  <Typography style="h5">Columns</Typography>
+                  {DEFAULT_TABLE_COLUMNS.map(({ data }, index) => (
+                    <Checkbox
+                      checked
+                      key={index}
+                      label={data.title}
+                      id={data.key}
+                    />
+                  ))}
+                </div>
+              </Dropdown>
+              <Button
+                icon={Plus}
+                iconPosition="right"
+                label="Add New Article"
+                className="bg-indigo-500"
+              />
+            </div>
 
-          <Typography style="h4" className="py-4">
-            67 Articles
-          </Typography>
-        </div>
+            <Typography style="h4" className="py-4">
+              67 Articles
+            </Typography>
+
+            <Scrollable>
+              <Table
+                rowSelection={false}
+                columnData={TABLE_COLUMNS}
+                rowData={data}
+                className="border border-red-500 "
+              />
+            </Scrollable>
+          </div>
+        </PageContainer>
       </div>
     </Container>
   );
