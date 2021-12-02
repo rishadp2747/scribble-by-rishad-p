b0025ba687d48c9d760e10d730a4d3d90a8fde45
rishadp <rishadp2747@gmail.com>
2021-12-02 11:23:43 +0530

    configured axios default and interceptor

---
+++ app/javascript/src/apis/axios.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { Toastr } from "neetoui";
+
+axios.defaults.baseURL = "/";
+
+axios.defaults.headers = {
+  "Content-Type": "application/json",
+  common: {
+    "X-CSRF-TOKEN": document.querySelector("[name=csrf-token]").content,
+  },
+};
+
+const handleSuccessResponse = response => {
+  if (response?.status === 200 && response?.data?.notice) {
+    Toastr.success(response.data.notice);
+  }
+
+  return response;
+};
+
+const handleErrorResponse = axiosErrorObject => {
+  const error = axiosErrorObject.response?.data?.error;
+  const default_error_message = "Something went wrong";
+  Toastr.error(Error(error || default_error_message));
+
+  return Promise.reject(axiosErrorObject);
+};
+
+export const setAxiosIntercepts = () => {
+  axios.interceptors.response.use(handleSuccessResponse, handleErrorResponse);
+};
