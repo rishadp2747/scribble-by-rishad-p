9415b0a417960dab0d136c04f3df238065c940a3
rishadp <rishadp2747@gmail.com>
2021-12-03 22:19:24 +0530

    Added articles test model

--- test/factories/articles.rb
+++ test/factories/articles.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :article do
-    
+    title { Faker::Book.title }
+    body { Faker::Lorem.paragraph }
+    status { "published" }
+    category
+    user
   end
 end
