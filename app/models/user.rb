69a630ebae6d3be63994b84391d537fc4e5057ae
rishadp <rishadp2747@gmail.com>
2021-12-03 22:15:33 +0530

    Adde user model and migration

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\. :a-z]+)*\.[a-z]+\z/i.freeze
+
+  has_many :articles
+  has_many :categories
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }
+
+  before_save :to_lowercase
+
+  private
+
+    def to_lowercase
+      email.downcase!
+    end
+end
