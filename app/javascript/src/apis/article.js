e174488ac45e4c2bd494b1ba34463e43d5edfa21
rishadp <rishadp2747@gmail.com>
2021-12-04 18:50:55 +0530

    cleaned

--- app/javascript/src/apis/article.js
+++ app/javascript/src/apis/article.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 const list = () => axios.get("articles");
+
 const show = articleId => axios.get(`articles/${articleId}`);
+
 const update = (articleId, payload) =>
   axios.put(`articles/${articleId}`, payload);
+
 const create = payload => axios.post("articles", payload);
+
 const destroy = articleId => axios.delete(`articles/${articleId}`);
 
 const articleApi = {
