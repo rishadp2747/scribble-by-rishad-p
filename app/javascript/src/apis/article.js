8b889e3973639133f3cd31475ee47811538181f5
rishadp <rishadp2747@gmail.com>
2021-12-04 16:03:27 +0530

    Added destroy api for articles

--- app/javascript/src/apis/article.js
+++ app/javascript/src/apis/article.js
@@ -2,9 +2,11 @@ import axios from "axios";
 
 const list = () => axios.get("articles");
 const create = payload => axios.post("articles", payload);
+const destroy = id => axios.delete(`articles/${id}`);
 
 const articleApi = {
   list,
   create,
+  destroy,
 };
 export default articleApi;
