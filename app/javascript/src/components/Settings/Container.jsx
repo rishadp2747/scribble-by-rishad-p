399e0e935c71e571dc75bf091993422ed7ce161d
rishadp <rishadp2747@gmail.com>
2021-12-06 20:48:32 +0530

    Added general setting form

--- app/javascript/src/components/Settings/Container.jsx
+++ app/javascript/src/components/Settings/Container.jsx
@@ -6,12 +6,12 @@ import SideMenu from "../Common/SideMenu";
 
 const Container = ({ children }) => {
   return (
-    <>
+    <div className="flex w-screen">
       <SideMenu>
         <NavBar />
       </SideMenu>
-      <div className="flex">{children}</div>
-    </>
+      <div className="flex w-full">{children}</div>
+    </div>
   );
 };
 
--- app/javascript/src/components/Settings/General.jsx
+++ app/javascript/src/components/Settings/General.jsx
@@ -1,7 +1,99 @@
 import React from "react";
 
+import { Formik, Form } from "formik";
+import { Close, Check } from "neetoicon";
+import { Typography, Button } from "neetoui";
+import { Input, Checkbox } from "neetoui/formik";
+
+import {
+  GENERAL_SETTINGS_FORM_INITIAL_VALUE,
+  GENERAL_SETTINGS_FORM_VALIDATION,
+} from "./constant";
+
 const General = () => {
-  return <div>general</div>;
+  const handleSubmit = () => {};
+
+  const passwordValiadtionIcon = (passwordValue, passwordErrors, message) => {
+    if (passwordValue === "" || passwordErrors?.includes(message)) {
+      return <Close size={15} color="#F56A58" />;
+    }
+
+    return <Check size={15} color="#00BA88" />;
+  };
+
+  return (
+    <div className="flex justify-center w-full py-8">
+      <div className="flex flex-col h-full space-y-6">
+        <div>
+          <Typography style="h1">General Settings</Typography>
+          <Typography style="body1">
+            Configure general attributes of scribble.
+          </Typography>
+        </div>
+        <Formik
+          onSubmit={handleSubmit}
+          initialValues={GENERAL_SETTINGS_FORM_INITIAL_VALUE}
+          validate={GENERAL_SETTINGS_FORM_VALIDATION}
+        >
+          {({ values, errors }) => (
+            <Form className="space-y-4">
+              <Input
+                required
+                type="text"
+                label="Site Name"
+                name="name"
+                helpText="Customize the site name which is used to show the site name in Open Graph Tags."
+              />
+              <div className="py-4 space-y-4">
+                <Checkbox
+                  name="isPassword"
+                  id="checkbox_name"
+                  label="Password Protect Knowledge Base"
+                />
+                {values.isPassword && (
+                  <>
+                    <Input
+                      type="password"
+                      label="Password"
+                      name="password"
+                      className="w-1/2"
+                      error={
+                        errors.password?.includes("Required") && "Required"
+                      }
+                    />
+                    <div className="flex items-center space-x-1">
+                      {passwordValiadtionIcon(
+                        values.password,
+                        errors.password,
+                        "Have at least 6 characters"
+                      )}
+                      <Typography style="body3">
+                        Have at least 6 characters
+                      </Typography>
+                    </div>
+                    <div className="flex items-center space-x-1">
+                      {passwordValiadtionIcon(
+                        values.password,
+                        errors.password,
+                        "Include at least 1 letter and 1 number"
+                      )}
+                      <Typography style="body3">
+                        Include at least 1 letter and 1 number
+                      </Typography>
+                    </div>
+                  </>
+                )}
+              </div>
+              <div className="flex flex-row space-x-2">
+                <Button type="submit" style="primary" label="Save Changes" />
+                <Button type="reset" style="text" label="Cancel" />
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
 };
 
 export default General;
