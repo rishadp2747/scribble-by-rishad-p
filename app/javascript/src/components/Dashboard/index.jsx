b7ac54536a480a27d3bb4996815693c68bb3ea02
rishadp <rishadp2747@gmail.com>
2021-12-06 10:24:04 +0530

    updated container logic and set route for settings

--- app/javascript/src/components/Dashboard/index.jsx
+++ app/javascript/src/components/Dashboard/index.jsx
@@ -9,7 +9,6 @@ import {
 } from "neetoui/layouts";
 
 import articleApi from "apis/article";
-import Container from "components/Common/Container";
 import CategoryMenu from "components/Dashboard/CategoryMenu";
 import {
   DEFAULT_TABLE_COLUMNS,
@@ -18,7 +17,7 @@ import {
 } from "components/Dashboard/constant";
 import useDebounce from "hooks/useDebounce";
 
-const Dashboard = ({ setLoading, loading }) => {
+const Dashboard = ({ setLoading }) => {
   const [articles, setArticles] = useState([]);
   const [articleCounts, setArticleCounts] = useState();
   const [filteredArticles, setFilteredArticles] = useState([]);
@@ -142,80 +141,78 @@ const Dashboard = ({ setLoading, loading }) => {
   };
 
   return (
-    <Container loading={loading}>
-      <div className="flex w-screen">
-        <MenuBar showMenu={true} title="Articles">
-          {STATUSES.map((status, index) => (
-            <MenuBar.Block
-              key={index}
-              label={status}
-              className="capitalize"
-              count={articleCounts && articleCounts[status]}
-              active={status === selectedFilters.status}
-              onClick={() => handleSelectedFilters({ status: status })}
-            />
-          ))}
-          <CategoryMenu
-            setLoading={setLoading}
-            selectedFilters={selectedFilters}
-            handleSelectedFilters={handleSelectedFilters}
+    <div className="flex w-screen">
+      <MenuBar showMenu={true} title="Articles">
+        {STATUSES.map((status, index) => (
+          <MenuBar.Block
+            key={index}
+            label={status}
+            className="capitalize"
+            count={articleCounts && articleCounts[status]}
+            active={status === selectedFilters.status}
+            onClick={() => handleSelectedFilters({ status: status })}
           />
-        </MenuBar>
-        <PageContainer>
-          <div className="flex flex-col w-full py-4">
-            <div className="flex flex-row w-4/5 h-8 ml-auto space-x-3">
-              <Input
-                size="small"
-                prefix={<Search size={20} />}
-                placeholder="Search article title"
-                value={selectedFilters.title}
-                onChange={e => handleSelectedFilters({ title: e.target.value })}
-              />
-              <Dropdown
-                closeOnSelect={false}
-                label="Columns"
-                buttonStyle="text"
-                position="bottom-end"
-                className="h-8 bg-gray-200 border"
-              >
-                <div className="p-4 space-y-4">
-                  <Typography style="h5">Columns</Typography>
-                  {DEFAULT_TABLE_COLUMNS.map(({ show, data }, index) => (
-                    <Checkbox
-                      checked={show}
-                      key={index}
-                      label={data.title}
-                      id={data.key}
-                      onChange={handleColumnChange}
-                    />
-                  ))}
-                </div>
-              </Dropdown>
-              <Button
-                icon={Plus}
-                iconPosition="right"
-                label="Add New Article"
-                className="bg-indigo-500"
-                to="/articles/creates"
-              />
-            </div>
-
-            <Typography style="h4" className="py-4">
-              {articles?.length} Articles
-            </Typography>
-
-            <Scrollable>
-              <Table
-                rowSelection={false}
-                columnData={TABLE_COLUMNS}
-                rowData={filteredArticles}
-                className="border border-red-500 "
-              />
-            </Scrollable>
+        ))}
+        <CategoryMenu
+          setLoading={setLoading}
+          selectedFilters={selectedFilters}
+          handleSelectedFilters={handleSelectedFilters}
+        />
+      </MenuBar>
+      <PageContainer>
+        <div className="flex flex-col w-full py-4">
+          <div className="flex flex-row w-4/5 h-8 ml-auto space-x-3">
+            <Input
+              size="small"
+              prefix={<Search size={20} />}
+              placeholder="Search article title"
+              value={selectedFilters.title}
+              onChange={e => handleSelectedFilters({ title: e.target.value })}
+            />
+            <Dropdown
+              closeOnSelect={false}
+              label="Columns"
+              buttonStyle="text"
+              position="bottom-end"
+              className="h-8 bg-gray-200 border"
+            >
+              <div className="p-4 space-y-4">
+                <Typography style="h5">Columns</Typography>
+                {DEFAULT_TABLE_COLUMNS.map(({ show, data }, index) => (
+                  <Checkbox
+                    checked={show}
+                    key={index}
+                    label={data.title}
+                    id={data.key}
+                    onChange={handleColumnChange}
+                  />
+                ))}
+              </div>
+            </Dropdown>
+            <Button
+              icon={Plus}
+              iconPosition="right"
+              label="Add New Article"
+              className="bg-indigo-500"
+              to="/articles/creates"
+            />
           </div>
-        </PageContainer>
-      </div>
-    </Container>
+
+          <Typography style="h4" className="py-4">
+            {articles?.length} Articles
+          </Typography>
+
+          <Scrollable>
+            <Table
+              rowSelection={false}
+              columnData={TABLE_COLUMNS}
+              rowData={filteredArticles}
+              className="border border-red-500 "
+            />
+          </Scrollable>
+        </div>
+      </PageContainer>
+    </div>
   );
 };
 
--- app/javascript/src/components/Main.jsx
+++ app/javascript/src/components/Main.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import { setAxiosInterceptor } from "apis/axios";
 import CreateArticle from "components/Article/CreateArticle";
 import EditArticle from "components/Article/EditArticle";
+import Container from "components/Common/Container";
 import Dashboard from "components/Dashboard";
+import Settings from "components/Settings";
 
 const Main = () => {
   const [loading, setLoading] = useState(false);
@@ -17,25 +19,37 @@ const Main = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route
-          exact
-          path="/"
-          render={() => <Dashboard setLoading={setLoading} loading={loading} />}
-        />
-        <Route
-          exact
-          path="/articles/creates"
-          render={() => (
-            <CreateArticle setLoading={setLoading} loading={loading} />
-          )}
-        />
-        <Route
-          exact
-          path="/articles/:articleId/edit"
-          render={() => (
-            <EditArticle setLoading={setLoading} loading={loading} />
-          )}
-        />
+        <Container loading={loading}>
+          <Redirect exact from="/" to={{ pathname: "/home" }} />
+          <Route
+            exact
+            path="/home"
+            render={() => (
+              <Dashboard setLoading={setLoading} loading={loading} />
+            )}
+          />
+          <Route
+            exact
+            path="/articles/creates"
+            render={() => (
+              <CreateArticle setLoading={setLoading} loading={loading} />
+            )}
+          />
+          <Route
+            exact
+            path="/articles/:articleId/edit"
+            render={() => (
+              <EditArticle setLoading={setLoading} loading={loading} />
+            )}
+          />
+          <Route
+            exact
+            path="/settings"
+            render={() => (
+              <Settings setLoading={setLoading} loading={loading} />
+            )}
+          />
+        </Container>
       </Switch>
     </BrowserRouter>
   );
