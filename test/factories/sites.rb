6f9bd94e18ee49cf5016d18065b4bb58fdd4ac6c
rishadp <rishadp2747@gmail.com>
2021-12-09 22:59:33 +0530

    Added site test

--- test/factories/sites.rb
+++ test/factories/sites.rb
@@ -2,6 +2,6 @@
 
 FactoryBot.define do
   factory :site do
-
+    name { Faker::Name.name }
   end
 end
--- test/models/site_test.rb
+++ test/models/site_test.rb
@@ -3,7 +3,42 @@
 require "test_helper"
 
 class SiteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @site = create(:site)
+  end
+
+  def test_site_should_be_valid
+    assert @site.valid?
+  end
+
+  def test_site_should_not_be_valid_without_name
+    @site.name = ""
+    assert_not @site.valid?
+    assert_includes @site.errors.full_messages, "Name can't be blank"
+  end
+
+  def test_site_should_not_be_valid_without_unique_name
+    duplicate_site = @site.dup
+    assert_not duplicate_site.valid?
+    assert_includes duplicate_site.errors.full_messages, "Name has already been taken"
+  end
+
+  def test_password_should_not_be_of_invalid_length
+    @site.password = "1a" * 2
+    assert_not @site.valid?
+    assert_includes @site.errors.full_messages, "Password is too short (minimum is 6 characters)"
+  end
+
+  def test_password_should_be_valid_of_valid_length
+    @site.password = "1a" * 3
+    assert @site.valid?
+  end
+
+  def test_password_should_not_be_of_invalid_format
+    invalid_passwords = %w[111111 aaaaaa]
+    invalid_passwords.each do |password|
+      @site.password = password
+      assert_not @site.valid?
+    end
+  end
 end
