662ce380b2a1f5be205e832a0087d35a7466ed0e
rishadp <rishadp2747@gmail.com>
2021-12-12 22:21:31 +0530

    Added slug to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -12,11 +12,27 @@ class Article < ApplicationRecord
   validates_associated :category, on: :create
   validates_associated :user
 
+  before_create :set_slug!
+
   def convert_date_format
     self.updated_at.strftime("%B %d, %Y")
   end
 
-  def find_unique_category_from_articles(articles)
-    @articles.uniq { |article| article.category.id }
-  end
+  private
+
+    def set_slug!
+      title_slug = title.parameterize
+      regex_pattern = "slug #{Constants::DB_REGEX_OPERATOR} ?"
+      latest_article_slug = Article.where(
+        regex_pattern,
+        "#{title_slug}$|#{title_slug}-[0-9]+$").order("LENGTH(slug) DESC", slug: :desc).first&.slug
+      slug_count = 0
+      if latest_article_slug.present?
+        slug_count = latest_article_slug.split("-").last.to_i
+        only_one_slug_exists = slug_count == 0
+        slug_count = 1 if only_one_slug_exists
+      end
+      slug_candidate = slug_count.positive? ? "#{title_slug}-#{slug_count + 1}" : title_slug
+      self.slug = slug_candidate
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_100006) do
+ActiveRecord::Schema.define(version: 2021_12_12_154919) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_12_12_100006) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
     t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
