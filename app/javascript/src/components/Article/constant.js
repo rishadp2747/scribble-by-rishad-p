41d2742963ba9eb69aa28b1ac07f8df7cd0f7bb3
rishadp <rishadp2747@gmail.com>
2021-12-14 22:35:29 +0530

    Added validation for long title article

--- app/javascript/src/components/Article/constant.js
+++ app/javascript/src/components/Article/constant.js
@@ -8,7 +8,7 @@ export const ARTICLE_FORM_INITIAL_VALUE = {
 };
 
 export const ARTICLE_FORM_VALIDATION_SCHEMA = yup.object().shape({
-  title: yup.string().trim().required("Required"),
+  title: yup.string().trim().required("Required").max(80, "Title is too long"),
   category: yup.object().required("Required").nullable(),
   body: yup.string().trim().required("Required"),
   status: yup.string().required("Required"),
