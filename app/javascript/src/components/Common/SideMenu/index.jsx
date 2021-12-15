0d217a45e091ba50ff3fa5fea40870fa89cab746
rishadp <rishadp2747@gmail.com>
2021-12-15 11:50:09 +0530

    updated and cleaned

--- app/javascript/src/components/Common/SideMenu/index.jsx
+++ app/javascript/src/components/Common/SideMenu/index.jsx
@@ -4,7 +4,7 @@ const SideMenu = ({ children }) => {
   const SIDE_MENU_STYLE = { width: "384px", backgroundColor: "white" };
 
   return (
-    <div className="border-r neeto-ui-menubar__wrapper" style={SIDE_MENU_STYLE}>
+    <div className="neeto-ui-menubar__wrapper" style={SIDE_MENU_STYLE}>
       <div
         className="space-y-4 neeto-ui-menubar__container"
         style={SIDE_MENU_STYLE}
