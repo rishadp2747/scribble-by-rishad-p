ce5ce57196622cb1bdee7cae770f25327a8ff772
rishadp <rishadp2747@gmail.com>
2021-12-20 09:31:07 +0530

    removed unwanted lines

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,17 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
-
     namespace :public do
       resource :session, only: :create
       resources :categories, only: :index
       resources :articles, only: %i[index show], param: :slug
     end
-
     resources :categories, except: %i[new edit] do
       put "sort", on: :collection
     end
-
     resource :site, only: %i[show update]
     resources :articles, except: %i[new edit]
     resources :redirections, except: %i[show new edit]
-
   end
 
   root "home#index"
